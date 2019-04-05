594c2ee2e182e8038533e6bfe9d2cc80a23a0b71
Bidur_Banstola <bidur.banstola@gmail.com>
2019-04-05 23:27:08 +0545

    Week 4

---
+++ src/Week4/SignalOperation/BankAccount.scala
@@ -0,0 +1,24 @@
+package Week4.SignalOperation
+
+class BankAccount {
+
+  val balance = Var(0)
+
+  def deposit(amount: Int): Unit = {
+    assert(amount > 0)
+    val b = balance()
+    balance() = b + amount
+  }
+
+  def withdraw(amount: Int): Int = {
+    assert(amount > 0)
+    val b = balance()
+    assert(b >= amount)
+    balance() = b - amount
+    balance()
+  }
+
+  def currentBalance(): Int = balance()
+
+  override def toString(): String = "Balance= "+balance()
+}
